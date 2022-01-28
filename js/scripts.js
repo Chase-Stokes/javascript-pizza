cf3cd33418d0087be8807430f9b7a8dc7195618c
Chase Stokes <chasehunterstokes@gmail.com>
2022-01-28 08:35:09 -0800

    add JS to pass first test

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function Pizza(price, size, toppings, premium) {
+  this.price = price;
+  this.size = size;
+  this.toppings = toppings;
+  this.premium = premium;
+}
+
+Pizza.prototype.adjustCost = function () {
+
+}
